566f20c8a48ebce7f1cf9cedbc89b1423958928f
Rajat Sen <rsen91@gmail.com>
2015-01-21 22:05:31 -0600

    indentation pain fixed

--- Src/fourier_trial.py
+++ Src/fourier_trial.py
@@ -7,13 +7,12 @@ import matplotlib.pyplot as plt
 def compute_diff(trip_list):
 	num_trips = len(trip_list);
 	trip_list_diff = [];
-    for i in range(len(trip_list)):
-    	curr_trip = trip_list[i];
-    	curr_trip = np.diff(curr_trip);
-    	curr_trip = np.diff(curr_trip);
-    	trip_list_diff.append(curr_trip);
-    return trip_list_diff;
-
+	for i in range(num_trips):
+		curr_trip = trip_list[i];
+		curr_trip = np.diff(curr_trip);
+		curr_trip = np.diff(curr_trip);
+		trip_list_diff.append(curr_trip);
+	return trip_list_diff
 
 # This function outputs a list of arrays with all arrays being of the same size
 def normalize_trip_list(trip_list):
@@ -36,6 +35,10 @@ def compute_transform(trip_list):
 	for i in range(num_trips):
 		transform_list[i] = numpy.fft.fft(trip_list[i]);
 	return transform_list
+
+
+if __name__ == "__main__":
+	print "test"
 
 
 
