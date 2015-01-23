1b66b8631fab79236912b3a085f266e5c2445977
Rajat Sen <rsen91@gmail.com>
2015-01-22 20:36:27 -0600

    test set creation code added

--- Src/parse1.py
+++ Src/parse1.py
@@ -1,9 +1,15 @@
 import csv
+import random
 
+valid_list = [];
 
+#Function to parse a particular trip of a driver
 def parsetrip(driver,trip):
 	s = '../../Data/drivers/' + str(driver) + '/' + str(trip) +'.csv';
-	f = open(s,'r');
+	try:
+		f = open(s,'r');
+	except:
+		return 0;
 	csv_f = csv.reader(f);
 	trip_list = [];
 	count = 0;
@@ -17,7 +23,7 @@ def parsetrip(driver,trip):
 	f.close();
 	return trip_list;
 
-
+#Function to parse a driver 
 def parsedriver(driver):
 	driver_list = [];
 	for i in range(200):
@@ -26,13 +32,41 @@ def parsedriver(driver):
 	return driver_list;
 
 
+#function to update the global variable with a valid_list of drivers
+def valid_drivers():
+	for i in range(3700):
+		s = '../../Data/drivers/' + str(i+1) + '/' + str(1) +'.csv';
+		try:
+			f = open(s,'r');
+		except:
+			continue;
+		valid_list.append(i+1);
+	return valid_list;
+
+
+#function to create a test set, where num trips from driver 'driver' is present and (total - num) trips of random drivers are added at the end.
+def createteset(driver, num,total):
+	main_list =  parsedriver(driver);
+	trip_list = [];
+	my_sample = random.sample(range(200), num);
+	for i in range(num):
+		trip_list.append(main_list[my_sample[i]]);
+	for i in range(total - num):
+		a = random.randrange(0, len(valid_list));
+		b = random.randrange(1,201);
+		trip = parsetrip(valid_list[a],b);
+		trip_list.append(trip);
+	return trip_list;
 
 
 
+
+#main method for testing the code
 if __name__ == "__main__":
 
-	driver = parsedriver(1);
-	print len(driver);
-	for i in range(len(driver)):
-		for j in range(10):
-			print driver[i][j], ' ',
+	valid_list = valid_drivers();
+	trip_list = createteset(1,6,20);
+	for i  in range(len(trip_list)):
+		print trip_list[i][0:10];
+
+
